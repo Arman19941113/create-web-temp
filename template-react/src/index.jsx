a9b8df12de7565b0b6dfd08f1dedffa73b4e4039
Arman19941113 <td19941113@gmail.com>
2021-02-15 21:24:15 +0800

    chore: update template-react

---
+++ template-react/src/index.jsx
@@ -0,0 +1,12 @@
+import 'react-hot-loader'
+import '@/css/index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
